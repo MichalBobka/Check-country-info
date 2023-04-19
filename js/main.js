56f643de0aa6b16e3b007007bf1e4f4a57097b6e
Michał <bobkamichal@gmail.com>
2023-04-19 18:04:06 +0200

    added files

---
+++ js/main.js
@@ -0,0 +1,89 @@
+const API_URL = 'https://restcountries.com/v3.1/all'
+let countries
+let query = ''
+let region = ''
+
+fetch(API_URL)
+	.then(res => res.json())
+	.then(countriesRaw => {
+		countries = countriesRaw.map(country => {
+			return {
+				capital: country.capital && country.capital[0],
+				population: country.population,
+				name: country.name.common,
+				region: country.region,
+				flagUrl: country.flags.png,
+			}
+		})
+		renderCountriesList(countries)
+	})
+const filteredDataAndRenderedCoutriesList = () => {
+	const filteredCountries = countries.filter(country => {
+		return country.name.toLowerCase().includes(query) && (!region || country.region === region)
+	})
+	renderCountriesList(filteredCountries)
+}
+document.querySelector('#query').addEventListener('input', e => {
+	query = e.target.value.toLowerCase().trim()
+	filteredDataAndRenderedCoutriesList()
+})
+
+document.querySelector('#region').addEventListener('change', e => {
+	region = e.target.value
+	filteredDataAndRenderedCoutriesList() 
+})
+
+const createInfoElement = (labelName, value) => {
+	const infoElement = document.createElement('div')
+	const labelElement = document.createElement('strong')
+	labelElement.innerText = `${labelName}: `
+	const valueElement = document.createElement('span')
+	valueElement.innerText = value
+
+	infoElement.append(labelElement, valueElement)
+
+	return infoElement
+}
+
+const createFlagImgElement = country => {
+	const imgContainerElement = document.createElement('div')
+	const imgElement = document.createElement('img')
+	imgElement.src = country.flagUrl
+	imgElement.alt = `${country.name} flag`
+	imgContainerElement.append(imgElement)
+
+	return imgContainerElement
+}
+
+const createCountryItemElement = country => {
+	const countryElement = document.createElement('li')
+
+	countryElement.append(createFlagImgElement(country))
+	const infoContainerElement = document.createElement('div')
+	infoContainerElement.classList.add('info-container')
+
+	const countryNameElement = document.createElement('strong')
+	countryNameElement.innerText = country.name
+	countryNameElement.classList.add('country-name')
+
+	infoContainerElement.append(countryNameElement)
+	infoContainerElement.append(createInfoElement('Population', country.population))
+	infoContainerElement.append(createInfoElement('Region', country.region))
+	infoContainerElement.append(createInfoElement('Capital', country.capital))
+	countryElement.append(infoContainerElement)
+
+	return countryElement
+}
+
+const createListElement = countries => {
+	const listElement = document.createElement('ul')
+	countries.forEach(country => {
+		listElement.append(createCountryItemElement(country))
+	})
+	return listElement
+}
+const renderCountriesList = countries => {
+	const rootElement = document.querySelector('#main')
+	rootElement.innerHTML = ''
+	rootElement.append(createListElement(countries))
+}
